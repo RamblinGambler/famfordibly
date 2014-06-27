5d6b35dca97c6829c2aca1c8c1aa2ae6fb46261c
Gamble McAdam <gamble.mcadam@gmail.com>
2014-06-27 15:26:54 -0500

    turn api call into resource

--- app/scripts/services/fuse.js
+++
@@ -1,3 +0,0 @@
-Affordably.service('institutions', function (array, options) {
-  return new Fuse(array, options);
-});
---
+++ app/scripts/services/institutions.js
@@ -0,0 +1,3 @@
+Affordably.factory('institutions', ['$resource', function($resource) {
+        return $resource('http://localhost:3000//api/v1/institutions')}
+]);
